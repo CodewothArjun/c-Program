b6aa95bfc1f3e2342eac291e15c2c29081e712c3
Arjun Rokkha <arjunrokka@gmail.com>
2022-02-01 12:02:24 +0545

    filehandel

--- file.c
+++ file.c
@@ -4,11 +4,17 @@ int main()
     FILE *p;
     p = fopen("Arjun.txt", "w");
     char name[30];
-    int roll;
-    printf("Enter a name and roll no.");
-    scanf("%s %d", &name, &roll);
-    fprintf(p, "%s\t %d\n", name, roll);
-    printf("%s\t %d\n", name, roll);
+    int age, roll, i, n;
+    printf("Enter a no. of student: ");
+    scanf("%d", &n);
+    
+    for (i = 0; i < n; i++)
+    {
+        printf("Enter a name and roll no. and age: ");
+        scanf("%s %d %d", &name, &roll, &age);
+        fprintf(p, "%s\t %d\t %d\n", name, roll, age);
+        printf("%s\t %d\t %d\n", name, roll, age);
+    }
     fclose(p);
     return 0;
 }

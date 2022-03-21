5a2562e7ddb3f7d42bede8d3bd8b6ca90bb8e636
Arjun Rokkha <arjunrokka@gmail.com>
2022-03-21 17:18:23 +0545

    update

---
+++ Digital Marketing/read.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+
+    // declearing the types of data
+    char f_name[50], l_name[50], password[50], email[30];
+    // file and mode
+    FILE *create;
+    create = fopen("register.txt", "r");
+
+    // Printing the output
+    fprintf(create, "First_Name\tLast_Name\tEmail\t\t\t\t\tPassword \n");
+    fprintf(create, "%s\t\t%s\t\t%s\t%s \n", f_name, l_name, email, password);
+}
